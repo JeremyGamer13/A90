7fab107148bb5c221a52e75b7b8e3c92c9ae1d1c
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-03-12 13:58:50 -0600

    fixes and other stuff

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { app, BrowserWindow, dialog, ipcMain, Tray } = require('electron')
 const electron = require('electron')
 const path = require('path')
-
 const psnode = require('ps-node')
-const findProcess = require('find-process')
+
+const IsDeveloperMode = false
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -17,7 +17,7 @@ function createWindow() {
         alwaysOnTop: true,
         transparent: true,
         webPreferences: {
-            devTools: false,
+            devTools: IsDeveloperMode,
             sandbox: false,
             spellcheck: false,
             preload: path.join(__dirname, 'preload.js')
@@ -26,7 +26,7 @@ function createWindow() {
 
     win.loadFile('index.html')
     win.maximize()
-    win.removeMenu()
+    if (!IsDeveloperMode) win.removeMenu()
     return win
 }
 
@@ -64,60 +64,12 @@ ipcMain.handle("started", (_, __) => {
 ipcMain.handle("getMousePos", (_, __) => {
     return electron.screen.getCursorScreenPoint()
 })
-
-const killablePrograms = []
-let programsAttempted = false
-
-function ResetKillablePrograms() {
+ipcMain.handle("killProgram", (_, pid) => {
     return new Promise((resolve, reject) => {
-        psnode.lookup('', (err, programs) => {
-            if (err) return reject(err)
-            killablePrograms.splice(0, killablePrograms.length)
-            for (let i = 0; i < programs.length; i++) {
-                const program = programs[i]
-                if (String(program.command).toLowerCase().endsWith("electron.exe")) continue // dont kill yourself please
-                if (String(program.command).toLowerCase().endsWith("a90.exe")) continue // dont kill yourself please
-                if (String(program.command).toLowerCase().endsWith("node.exe")) continue // dont kill yourself please
-
-                if (String(program.command).toLowerCase().includes("system32")) continue // we are evil, not evil enough to kill a system32 process lol!
-                if (String(program.command).toLowerCase().includes("windowsapps")) continue // idk what this is
-                if (String(program.command).toLowerCase().includes("systemapps")) continue // idk what this is
-                if (String(program.command).toLowerCase().includes("microsoft")) continue // idk what this is
-                if (String(program.command).toLowerCase().includes("java\\java")) continue // idk what this is
-                if (String(program.command).toLowerCase().includes("steamwebhelper")) continue // idk what this is
-                if (String(program.command).toLowerCase().substring(0, 12).includes("windows")) continue // idk what this is
-                if (String(program.command).substring(1, 3) != ":\\") continue // be safe
-                killablePrograms.push(program)
-            }
-            resolve(killablePrograms)
+        psnode.kill(pid, {
+            timeout: 1,
+        }, err => {
+            resolve(err)
         })
     })
-}
-
-app.on("ready", () => {
-    ResetKillablePrograms().then(() => {
-        programsAttempted = true
-    }).catch(() => {
-        programsAttempted = true
-        dialog.showMessageBox({ message: "A-90 can't grab your open windows! You are safe for now." })
-    })
-})
-ipcMain.handle("killRandomProgram", (_, __) => {
-    const index = Math.round(Math.random() * (killablePrograms.length - 1))
-    const program = killablePrograms[index]
-    if (!program) return console.log("damn, nothing to kill")
-    console.log('DIE', program.command, "!")
-    psnode.kill(program.pid, (err) => {
-        if (err) {
-            if (!(String(err).includes("Kill process timeout"))) killablePrograms.splice(index, 1)
-            return
-        }
-        killablePrograms.splice(index, 1)
-    })
-})
-ipcMain.handle("programsAttempted", (_, __) => {
-    return programsAttempted
-})
-ipcMain.handle("resetKillablePrograms", (_, __) => {
-    ResetKillablePrograms()
 })
--- preload.js
+++ preload.js
@@ -1,4 +1,54 @@
 const { ipcRenderer } = require("electron");
+const { uIOhook } = require('uiohook-napi')
+const psnode = require('ps-node')
+
+const killablePrograms = []
+let programsAttempted = false
+
+function ResetKillablePrograms() {
+    return new Promise((resolve, reject) => {
+        psnode.lookup({
+            command: "([A-Za-z]:\\\\)(?!window)"
+        }, (err, programs) => {
+            if (err) return reject(err)
+            killablePrograms.splice(0, killablePrograms.length)
+            for (let i = 0; i < programs.length; i++) {
+                const program = programs[i]
+                const commandCheck = String(program.command).toLowerCase().replace(/\//gi, "\\")
+                if (commandCheck.endsWith("electron.exe")) continue // dont kill yourself please
+                if (commandCheck.endsWith("a90.exe")) continue // dont kill yourself please
+                if (commandCheck.endsWith("node.exe")) continue // dont kill yourself please
+
+                if (commandCheck.includes("system32")) continue // we are evil, not evil enough to kill a system32 process lol!
+                if (commandCheck.includes("windowsapps")) continue // idk what this is
+                if (commandCheck.includes("systemapps")) continue // idk what this is
+                if (commandCheck.includes("microsoft")) continue // idk what this is
+                if (commandCheck.includes("java\\java")) continue // idk what this is
+                if (commandCheck.includes("steamwebhelper")) continue // idk what this is
+                if (commandCheck.substring(0, 12).includes("windows")) continue // idk what this is
+
+                if (commandCheck.includes("obs-studio")) continue // dont close obs so you can record it (because what else would you record with)
+                killablePrograms.push(program)
+            }
+            console.log(killablePrograms)
+            resolve(killablePrograms)
+        })
+    })
+}
+function KillRandomProgram() {
+    const index = Math.round(Math.random() * (killablePrograms.length - 1))
+    const program = killablePrograms[index]
+    if (!program) return console.log("damn, nothing to kill")
+    console.log('DIE', program.command, "!")
+    ipcRenderer.invoke("killProgram", String(program.pid)).then(err => {
+        if (err) {
+            if (!(String(err).includes("Kill process timeout"))) killablePrograms.splice(index, 1)
+            return
+        }
+        killablePrograms.splice(index, 1)
+    })
+}
+
 window.addEventListener("DOMContentLoaded", () => {
     function WaitForBegin() {
         return new Promise((resolve, reject) => {
@@ -14,18 +64,76 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     const peepoo = setInterval(() => {
-        ipcRenderer.invoke("programsAttempted").then(ye => {
-            if (ye) {
-                document.getElementById("deeznutslmao!").remove()
-                clearInterval(peepoo)
-            }
-        })
+        if (programsAttempted) {
+            document.getElementById("deeznutslmao!").remove()
+            clearInterval(peepoo)
+        }
     }, 500)
+
+    const _cacheAudio = new Audio("jumpscare.mp3") // cache audio so it can load faster maybe
 
     WaitForBegin().then(options => {
         ipcRenderer.invoke("started")
         document.body.innerHTML = ""
-        if (options.interval < 0 || options.chance < 0 || options.chance > 100) {
+        if (options.chance == 0) { // rush
+            ipcRenderer.invoke("fullscreen")
+            const rush = document.createElement("img")
+            rush.style.position = "absolute"
+            let size = 15
+            rush.style.left = `calc(50% - ${size / 2}%)`
+            rush.style.top = `calc(50% - ${(size + (size / 1.5)) / 2}%)`
+            rush.style.width = `${size}%`
+            rush.style.height = `${size + (size / 1.5)}%`
+            rush.src = "RushNormal.webp"
+            document.body.append(rush)
+            let changeBy = 1
+            document.body.style.backgroundColor = "black"
+            const audio = new Audio("rush_static.mp3")
+            audio.currentTime = 0
+            audio.play()
+            audio.onended = () => {
+                audio.src = "rush_static2.mp3"
+                document.body.style.backgroundColor = "rgb(0, 0, 12)"
+                audio.currentTime = 0
+                audio.play()
+                size = 20
+                rush.style.left = `calc(50% - ${size / 2}%)`
+                rush.style.top = `calc(50% - ${(size + (size / 1.5)) / 2}%)`
+                rush.style.width = `${size}%`
+                rush.style.height = `${size + (size / 1.5)}%`
+                audio.onended = () => {
+                    audio.src = "rush_static3.mp3"
+                    document.body.style.backgroundColor = "rgb(0, 0, 26)"
+                    audio.currentTime = 0
+                    audio.play()
+                    size = 24
+                    rush.style.left = `calc(50% - ${size / 2}%)`
+                    rush.style.top = `calc(50% - ${(size + (size / 1.5)) / 2}%)`
+                    rush.style.width = `${size}%`
+                    rush.style.height = `${size + (size / 1.5)}%`
+                    audio.onended = () => {
+                        rush.src = "RushAttack.webp"
+                        audio.src = "rush_jumpscare.mp3"
+                        audio.currentTime = 0
+                        audio.play()
+                        setInterval(() => {
+                            document.body.style.backgroundColor = `rgb(0, ${(Math.random() * 20) + 30}, ${(Math.random() * 80) + 120})`
+                            size += changeBy
+                            changeBy += 3
+                            rush.style.left = `calc((50% - ${size / 2}%) + ${(Math.random() * 16) - 8}px)`
+                            rush.style.top = `calc((50% - ${(size + (size / 1.5)) / 2}%) + ${(Math.random() * 16) - 8}px)`
+                            rush.style.width = `${size}%`
+                            rush.style.height = `${size + (size / 1.5)}%`
+                        }, 5)
+                        audio.onended = () => {
+                            ipcRenderer.invoke("quitApp")
+                        }
+                    }
+                }
+            }
+            return
+        }
+        if (options.interval < 0 || options.chance < 0 || options.chance > 100) { // ambush
             ipcRenderer.invoke("fullscreen")
             const audio = new Audio("ambush_jumpscare.mp3")
             audio.play()
@@ -66,7 +174,7 @@ window.addEventListener("DOMContentLoaded", () => {
                         switchedYet = true
                     } else {
                         size += changeBy
-                        changeBy += 2
+                        changeBy += 4
                     }
                     ambush.style.left = `calc(50% - ${size / 2}%)`
                     ambush.style.top = `calc(50% - ${size}%)`
@@ -82,7 +190,29 @@ window.addEventListener("DOMContentLoaded", () => {
             mouse: false,
             keyboard: false,
         }
+        uIOhook.on("mousemove", () => {
+            UserInteraction.mouse = true
+        })
+        uIOhook.on("mousedown", () => {
+            UserInteraction.mouse = true
+        })
+        uIOhook.on("wheel", () => {
+            UserInteraction.mouse = true
+        })
+        uIOhook.on('keydown', () => {
+            UserInteraction.keyboard = true
+        })
+        setInterval(() => {
+            UserInteraction.mouse = false
+            UserInteraction.keyboard = false
+        }, 20)
+
+        uIOhook.start()
+
+        let canAttackUser = true
         function attackUser() {
+            if (!canAttackUser) return
+            canAttackUser = false
             const audio = new Audio("jumpscare.mp3")
             audio.play()
             const a90 = document.createElement("img")
@@ -104,6 +234,7 @@ window.addEventListener("DOMContentLoaded", () => {
             document.body.append(a90stop)
             let moved = false
             let checkDone = false
+            let shouldKillProgram = false
             const interval = setInterval(() => {
                 if (audio.currentTime > 0.52) {
                     document.body.style.backgroundColor = "black"
@@ -129,12 +260,13 @@ window.addEventListener("DOMContentLoaded", () => {
                         a90.remove()
                         a90stop.remove()
                         clearInterval(interval)
+                        canAttackUser = true
                         checkDone = true
                         return
                     }
                     if (!checkDone) {
                         if (options.canCloseWindows) {
-                            ipcRenderer.invoke("killRandomProgram")
+                            shouldKillProgram = true
                         }
                         checkDone = true
                     }
@@ -150,6 +282,12 @@ window.addEventListener("DOMContentLoaded", () => {
                 if (audio.currentTime > 2.25) {
                     a90.style.display = "none"
                     document.body.style.backgroundColor = "red"
+                    if (shouldKillProgram) {
+                        if (options.canCloseWindows) {
+                            KillRandomProgram()
+                        }
+                        shouldKillProgram = false
+                    }
                 }
                 if (audio.currentTime > 2.3) {
                     a90.style.display = "none"
@@ -162,30 +300,22 @@ window.addEventListener("DOMContentLoaded", () => {
             }, 5)
             audio.addEventListener("ended", () => {
                 clearInterval(interval)
+                canAttackUser = true
             })
         }
-        const mouseCursor = {
-            x: 0,
-            y: 0
-        }
-        setInterval(() => {
-            ipcRenderer.invoke("getMousePos").then(val => {
-                UserInteraction.mouse = ((val.x != mouseCursor.x) || (val.y != mouseCursor.y))
-                mouseCursor.x = val.x
-                mouseCursor.y = val.y
-            })
-            UserInteraction.keyboard = false // not implemented yet lol!
-        }, 20)
 
         setInterval(() => {
-            if (Math.round(Math.random() * 100) >= (100 - options.chance)) {
+            if ((Math.round(Math.random() * 99) + 1) > (100 - options.chance)) {
                 attackUser()
             }
         }, options.interval)
-        if (options.canCloseWindows) {
-            setInterval(() => {
-                ipcRenderer.invoke("resetKillablePrograms")
-            }, 10000);
-        }
+    })
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+    ResetKillablePrograms().then(() => {
+        programsAttempted = true
+    }).catch(() => {
+        programsAttempted = true
     })
 })
